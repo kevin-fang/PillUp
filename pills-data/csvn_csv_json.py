2bdd073fc6040477745ee09a792c99fc2300345f
mmohades <mmohades@hotmail.com>
2018-09-15 09:03:35 -0400

    Arduino code for ultra sound sensor and low level trigger

--- pills-data/csvn_csv_json.py
+++ pills-data/csvn_csv_json.py
@@ -1,15 +1,3 @@
-import csv
-import json
-
-csvfile = open('pillbox.csv', 'r')
-jsonfile = open('file.json', 'w')
-
-fieldnames = ("id", "spl_id","SETID","spp", "NDC9", "PRODUCT_CODE","EQUAL_PRODUCT_CODE",
-              "author", "SPLIMPRINT" ,"SPLCOLOR" ,"SPLSHAPE", "SPLSIZE" ,"SPLSCORE" ,"DEA_SCHEDULE_CODE",
-              "INGREDIENTS", "SPL_INACTIVE_ING", "RXCUI,RXTTY", "RXSTRING", "image_id", "IMAGE_SOURCE",
-              "HAS_IMAGE","FROM_SIS","NO_RXCUI")
-
-reader = csv.DictReader( csvfile, fieldnames)
-for row in reader:
-    json.dump(row, jsonfile)
-    jsonfile.write('\n')
+from mongoengine import connect
+connect('pillsup')
+test = connect(host='mongodb://admin:admin9090@ds257732.mlab.com:57732/pillsup')
