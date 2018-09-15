6392333f1031d81ab34173062dfbcfd2b14d645a
Shayan <sh.taslim@gmail.com>
2018-09-15 12:23:41 -0400

    updated host

--- Server/main.py
+++ Server/main.py
@@ -3,7 +3,7 @@ from app import create_app, socketio
 
 def main():
     app = create_app()
-    socketio.run(app, port=8080)
+    socketio.run(app, host='0.0.0.0', port=8080)
 
 if __name__ == '__main__':
     main()
