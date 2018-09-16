9415d94060440f6564e65b9d6d3f6ec44de6ee6c
mmohades <mmohades@hotmail.com>
2018-09-16 00:35:14 -0400

    Rasp file controlling servos is done

--- Rasp/main.py
+++ Rasp/main.py
@@ -1,49 +1,59 @@
 from gpiozero import DistanceSensor, AngularServo
 from time import sleep
 
-sensor = DistanceSensor(echo=17, trigger=4, max_distance=200)
-serv_right = AngularServo(4)
-serv_left = AngularServo(27)
-# serv_sticker = AngularServo(25)
+#Configuring servos with pin numbers
+serv_right = AngularServo(27)
+serv_left = AngularServo(17)
+serv_sticker = AngularServo(24)
 
-
+#Put them at the init place
 serv_right.angle = -3
 serv_left.angle = -5
-# serv_sticker.angle = 90
-# sleep(3)
-# serv_sticker.angle = -40
+sleep(1)
+serv_sticker.angle = -80
+sleep(1)
 
 
 def cart_1():
+    """
+        This is the left servo
 
-    serv_right.angle = -90
-    sleep(1)
-
-    serv_right.angle = -3
-    sleep(1)
-
-def cart_2():
+    """
     serv_left.angle = 90
     sleep(1)
 
     serv_left.angle = -5
     sleep(1)
 
+def cart_2():
+        """
+            This is the right servo
 
-state = False
+        """
+    serv_right.angle = -90
+    sleep(1)
 
-while True:
+    serv_right.angle = -3
+    sleep(1)
 
 
-    distance = sensor.distance
-    if distance == 0:
-        distance = 400
-    print(distance)
 
-    if distance < 1 and state:
-        cart_1()
-        cart_2()
-        state = False
-    else:
-        # state = True
-        pass
+def like():
+    """
+        Raise sticker function
+    """
+    serv_sticker.angle = 85
+    sleep(2)
+    serv_sticker.angle = -80
+    sleep(2)
+
+
+# while True:
+
+#     # Testing servos. It should be deleted later on
+#     cart_1()
+#     sleep(1)
+
+#     cart_2()
+#     sleep(1)
+#     like()
