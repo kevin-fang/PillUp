caaad19ff37798180404dd4a609c4e768c348ecc
mmohades <mmohades@hotmail.com>
2018-09-16 03:02:24 -0400

    Arduino code for water pump is almost done

--- Arduino/servo_with_solar.ino
+++ Arduino/servo_with_solar.ino
@@ -1,18 +1,17 @@
 #include <Servo.h>
 
-int pos = 0;    // variable to store the servo position
-const int trigPin = 12;
-const int echoPin = 11;
-//const int pomp = 5;
-
-
+const int trigPin = 5;
+const int echoPin = 4;
+const int pomp = 10;
+int n;
+bool state = false;
 // defines variables
 long duration;
 int distance;
-Servo myservo;
+int counterOff = 0;
+int counterOn = 0;
+int calculate_distance() {
 
-int calculate_distance(){
-  
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
 
@@ -28,37 +27,45 @@ int calculate_distance(){
   distance = duration * 0.034 / 2;
 
   return distance;
-  
-  
-  }
+
+
+}
 
 void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-//  pinMode(pomp, OUTPUT);
-  myservo.attach(5);  // attaches the servo on pin 9 to the servo object
+  pinMode(pomp, OUTPUT);
   Serial.begin(9600);
-  
+
 }
 void loop() {
-  
-    calculate_distance();
 
-  if(distance < 7){
-    
-//      digitalWrite(pomp, HIGH);
-      myservo.write(50);
-      delay(1000);                      
-      myservo.write(-50);
-      delay(1000); 
-      
-      Serial.println("STOP");
+  calculate_distance();
+  Serial.println(distance);
 
+  if (distance < 4 && state ) {
+      counterOn++;
+      if(counterOn > 7)
+      {
+        delay(1000*1.5);
+        digitalWrite(pomp, HIGH);
+        delay(100);
+        state = false;
+        counterOn = 0;
+        Serial.println("Pump is on");
     }
-    else{
-//      digitalWrite(pomp, LOW);
-        delay(1000);
-      Serial.print("Distance: ");
-      Serial.println(distance);
+  }
+  else{
+    if(!state && distance > 8 )
+    {
+      counterOff++;
+  //    printf("Counter off value is %d", counterOff, "\n");
+      if (counterOff > 10){
+      state = true; 
+      counterOff = 0;
+      }
+  }
     }
+    digitalWrite(pomp, LOW);
+
 }
--- Rasp/servo.py
+++ Rasp/servo.py
@@ -1,37 +1,59 @@
-# from gpiozero import AngularServo, Motor
-# from time import sleep
-# from enum import Enum
-# import keyboard as kb
-#
-# s = AngularServo(17, min_angle=-45, max_angle=45)
-# m = Motor(27, 21, pwm=True)
-#
-# class Rotation(Enum):
-#    left = -45
-#    right = 45
-#    straight = 0
-#
-# def turn(rotation, degree = None):
-#
-#    if degree:
-#       s.angle = degree
-#    else:
-#       s.angle = rotation.value
-#
-#    straight = 0
-#
-# def turn(rotation, degree = None):
-#    right = 45
-#    if degree:
-#       s.angle = degree
-#    else:
-#       s.angle = rotation.value
-#
-# s.angle = 0.0
-# while True:
-#    m.forward(float(input()))
+from gpiozero import DistanceSensor, AngularServo
+from time import sleep
+
+#Configuring servos with pin numbers
+serv_right = AngularServo(27)
+serv_left = AngularServo(17)
+serv_sticker = AngularServo(24)
+
+#Put them at the init place
+serv_right.angle = -3
+serv_left.angle = -5
+sleep(1)
+serv_sticker.angle = -80
+sleep(1)
 
 
-import serial
+def cart_1():
+    """
+        This is the left servo
 
-arduinoSerialData = serial.Serial('/dev/cu.usbmodem14431', 9600)
+    """
+    serv_left.angle = 90
+    sleep(1)
+
+    serv_left.angle = -5
+    sleep(1)
+
+def cart_2():
+        """
+            This is the right servo
+
+        """
+    serv_right.angle = -90
+    sleep(1)
+
+    serv_right.angle = -3
+    sleep(1)
+
+
+
+def like():
+    """
+        Raise sticker function
+    """
+    serv_sticker.angle = 85
+    sleep(2)
+    serv_sticker.angle = -80
+    sleep(2)
+
+
+while True:
+
+    # Testing servos. It should be deleted later on
+    cart_1()
+    sleep(1)
+
+    cart_2()
+    sleep(1)
+    like()
